a16ac412a294156c6ac3fb0c5d336bfc8c9b3d0d
ToBiyo <fabio.web.arru@gmail.com>
2024-02-21 18:50:18 +0100

    router path fix

--- src/main.jsx
+++ src/main.jsx
@@ -28,12 +28,12 @@ const theme = extendTheme({colors});
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/InusulaConsult/" element={<App />}>
+    <Route path="/InsulaConsult/" element={<App />}>
       <Route path="/InsulaConsult/" element={<Home />} />
       <Route path="/InsulaConsult/about" element={<About />} />
       <Route path="/InsulaConsult/services" element={<Services />} />
-      <Route path="InsulaConsult/projects" element={<Projects />} />
-      <Route path="InsulaConsult/network" element={<Network />} />
+      <Route path="/InsulaConsult/projects" element={<Projects />} />
+      <Route path="/InsulaConsult/network" element={<Network />} />
     </Route>
   )
 );
